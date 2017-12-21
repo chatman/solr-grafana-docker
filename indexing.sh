3c21f4f7a2bfec8542b1a72780be530a50081753
Ishan Chattopadhyaya <ishan@apache.org>
2017-12-21 22:02:31 +0530

    Rolling restart test

--- indexing.sh
+++ indexing.sh
@@ -1,4 +1,4 @@
-curl "http://localhost:8983/solr/admin/collections?action=CREATE&name=mycoll2&numShards=2&replicationFactor=2"
+curl "http://localhost:8983/solr/admin/collections?action=CREATE&name=mycoll2&numShards=5&replicationFactor=2"
 for i in {1..1000000}
   do echo $i; curl "http://localhost:8983/solr/mycoll2/update" -X POST -d "[{'id':'id$i'}, {'id':'myid$i'}]" -H "Content-type: application/json"
 done
---
+++ rolling-restart.sh
@@ -0,0 +1,7 @@
+for i in {1..10}
+do
+   echo "Restarting solr$i"
+   docker exec solrgrafanadocker_solr${i}_1 /bin/bash -c "/opt/solr/bin/solr stop -all; /opt/solr/bin/solr -c -z zk1:2181 -m 4g"
+   sleep 2
+done
+

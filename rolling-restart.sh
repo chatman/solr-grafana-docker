46d6e5b370a22a198976147af19e38dca57ca05a
Ishan Chattopadhyaya <ishan@apache.org>
2018-01-03 22:20:27 +0530

    Not restarting the first node

--- rolling-restart.sh
+++ rolling-restart.sh
@@ -1,4 +1,4 @@
-for i in {1..10}
+for i in {2..10}
 do
    echo "Restarting solr$i"
    docker exec solrgrafanadocker_solr${i}_1 /bin/bash -c "/opt/solr/bin/solr stop -all; /opt/solr/bin/solr -c -z zk1:2181 -m 4g"

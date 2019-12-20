60a50024472cdf055995da62dd95a1fd0b702250
Ishan Chattopadhyaya <ishan@apache.org>
2019-12-20 22:36:39 +0530

    Smart vs Old Solr

--- indexing.sh
+++ indexing.sh
@@ -1,4 +1,12 @@
 curl "http://localhost:8983/solr/admin/collections?action=CREATE&name=mycoll2&numShards=2&replicationFactor=2"
-for i in {1..1000000}
-  do echo $i; curl "http://localhost:8983/solr/mycoll2/update" -X POST -d "[{'id':'id$i'}, {'id':'myid$i'}]" -H "Content-type: application/json"
+curl "http://localhost:7574/solr/admin/collections?action=CREATE&name=mycoll2&numShards=2&replicationFactor=2&externalState=true"
+
+for i in {1..2000};
+   do echo $i; curl "http://localhost:8983/solr/mycoll2/update?commit=true" -X POST -d "[{'id':'id$i'}, {'id':'myid$i'}]" -H "Content-type: application/json"; \
+      echo $i; curl "http://localhost:7574/solr/mycoll2/update?commit=true" -X POST -d "[{'id':'id$i'}, {'id':'myid$i'}]" -H "Content-type: application/json"; \
+      if ! (( $i % 20 )); then \
+          echo "RELOADING.."; \
+          curl "http://localhost:8983/solr/admin/collections?action=RELOAD&name=mycoll2"; \
+          curl "http://localhost:7574/solr/admin/collections?action=RELOAD&name=mycoll2"; \
+      fi
 done
